674e1aa89eaa66a3e87fe656abede102db57ff43
Reza Shah <33800875+iamrezashah@users.noreply.github.com>
2018-11-30 12:55:46 +0330

    Update and rename all - any.py to all_any.py

--- all_any.py
+++ all_any.py
@@ -7,10 +7,3 @@ if any(my_list):
 
 if all(my_list):
 	print('All of them is TRUE')
-
-
-import wikipedia
-result = wikipedia.page('python3')
-print(result.summary)
-for link in result.links:
-	print(link)
